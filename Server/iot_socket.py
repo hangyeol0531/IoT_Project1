702a15b414163c4947d092e72ca755eb642052e0
hangyeol0531 <jhan0531@naver.com>
2020-11-04 12:37:17 +0900

    Update iot_socket.py

--- Server/iot_socket.py
+++ Server/iot_socket.py
@@ -3,27 +3,25 @@ import serial
 import socket
 ser = serial.Serial('/dev/ttyACM1', 9600)
 
-HOST = '127.0.0.1'
-PORT = 8000
+# 통신 정보 설정
+IP = '127.0.0.1'
+PORT = 5050
+SIZE = 1024
+ADDR = (IP, PORT)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server_socket.bind((HOST, PORT))
-server_socket.listen()
-
-client_socket, addr = server_socket.accept()
-print('Connected by', addr)
+# 서버 소켓 설정
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind(ADDR)  # 주소 바인딩
+    server_socket.listen()  # 클라이언트의 요청을 받을 준비
 
 while True:
-    # 클라이언트가 보낸 메시지를 수신하기 위해 대기합니다. 
-    data = client_socket.recv(1024)
-    # 빈 문자열을 수신하면 루프를 중지합니다. 
-    if not data:
-        break
-    # 수신받은 문자열을 출력합니다.
-    print('Received from', addr, data.decode())
+        client_socket, client_addr = server_socket.accept()  # 수신대기, 접속한 클라이언트 정보 (소켓, 주소) 반환
+        msg = client_socket.recv(SIZE)  # 클라이언트가 보낸 메시지 반환
+        print("[{}] message : {}".format(client_addr,msg))  # 클라이언트가 보낸 메시지 출력
 
+        client_socket.sendall("welcome!".encode())  # 클라이언트에게 응답
+
+        client_socket.close()  # 클라이언트 소켓 종료
     if data == 'red_on': ser.write(b'1')
     elif data == 'red_on': ser.write(b'2')
     elif data == 'blue_on': ser.write(b'3');
@@ -31,11 +29,6 @@ while True:
     elif data == 'green_on': ser.write(b'5');
     elif data == 'green_off': ser.write(b'6');
     elif data == 'all_on': ser.write(b'7');
-    elif data == 'all_off': ser.write(b'8');
-    # 받은 문자열을 다시 클라이언트로 전송해줍니다.(에코) 
-    #client_socket.sendall(data)
-# 소켓을 닫습니다.
-client_socket.close()
-server_socket.close()
+    elif data == 'all_off'  : ser.write(b'8');
 
 
